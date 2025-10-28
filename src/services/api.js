f5885484214a9e654e8fd1bc9580746c53cc8fea
Ashish Kumar Mishra <ashishmishra6352@gmail.com>
2025-10-28 14:05:29 +0530

    project structured

--- src/services/api.js
+++ src/services/api.js
@@ -1,26 +1,11 @@
-import axios from "axios";
+import React from 'react'
 
-const API_URL= 'http://localhost:5000/api';
+const api = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-const api=axios.create({
-    baseURL:API_URL,
-    headers:{
-        'Content-Type':'application/json'
-    }
-});
-
-api.interceptors.request.use(
-    (config)=>{
-        const token=localStorage.getItem('token');
-        if(token){
-            config.headers.Authorization=`Bearer ${token}`
-        }
-        return config;
-    },
-
-    (error)=>{
-        return Promise.reject(error);
-    }
-);
-
-export default api;
+export default api
